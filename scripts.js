4464423ef0e2ae0ad2d200ad596e1b1e726729cc
jchaidez23 <josechaidez23@gmail.com>
2024-09-03 12:42:35 -0700

    Added class and new functions to calculator

--- scripts.js
+++ scripts.js
@@ -1,10 +1,34 @@
-function generateButtons(){
-    const result_div = document.querySelector("#calculator_container");
-    let row = document.createElement("div");
-    for(let i = 0; i < 4; i++){
-        row.setAttribute("style", "border: black 10px solid;")
-        row.className = "added_row";
-        result_div.appendChild(row);
+function getValue(num,text){
+    let operators = ["clear","+","-","/","*"];
+    if(operators.includes(text)){
+        if(text === "clear"){
+            clear();
+        }
+        else if(text === "+"){
+            add();
+        }
+        else if(text === "-"){
+            subtract();
+        }
+        else if(text === "/"){
+            divide();
+        }
+        else{
+            multiply();
+        }
     }
+    else{
+        num += text;
+    }
+    return 
 
+}
+
+function clear(num1, num2, operator){
+    /* Need to clear the screen of whatever number is there and then set the values to 0 */
+    num1 = "0";
+    num2 = "0";
+    operator = "";
+    let values = [num1, num2, operator];
+    return num1, num2, operator;
 }
