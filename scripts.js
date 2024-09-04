a09ede114f6a324794ea7ecf7ddefdc3c04a4c54
jchaidez13 <josechaidez23@gmail.com>
2024-09-03 21:49:30 -0700

    Fixed calculator functions, final step in progress

--- scripts.js
+++ scripts.js
@@ -1,34 +1,66 @@
-function getValue(num,text){
-    let operators = ["clear","+","-","/","*"];
-    if(operators.includes(text)){
-        if(text === "clear"){
-            clear();
-        }
-        else if(text === "+"){
-            add();
-        }
-        else if(text === "-"){
-            subtract();
-        }
-        else if(text === "/"){
-            divide();
+var num1 = "";
+var num2 = "";
+var operator = "";
+
+function calculate(num1,num2,operator){
+    switch(operator){
+        case '+':
+            console.log(parseInt(num1) + parseInt(num2));
+            break;
+        case '-':
+            console.log(parseInt(num1) - parseInt(num2));
+            break;
+        case '/':
+            console.log(parseInt(num1) / parseInt(num2));
+            break;
+        case 'X':
+            console.log(parseInt(num1) * parseInt(num2));
+            break;
+        case '%':
+            console.log(parseInt(num1) % parseInt(num2));
+            break;
+    }
+}
+
+function getValue(text){
+    let class_name = text;
+    console.log(class_name);
+    var element = document.getElementById(class_name).textContent;
+    console.log(element);
+    const operators = ["+","-","X","/","%"];
+    if (operators.includes(element) && operator == ""){
+        operator = element;
+    }
+    else if(operators.includes(operator)){
+        
+        if(element == "="){
+            calculate(num1,num2,operator);
         }
         else{
-            multiply();
+            num2 += element;
+            console.log("Num2:" + num2);    
         }
     }
     else{
-        num += text;
+            num1 += element;
+            console.log("Num1" + num1);
     }
-    return 
+    
 
 }
 
-function clear(num1, num2, operator){
+function display(string){
+    const div = document.querySelector("#results_row");
+    let display = document.createElement("p");
+    display.textContent = string;
+    display.setAttribute("style", "color: white;");
+    div.appendChild(display)
+}
+
+function clearValues(){
     /* Need to clear the screen of whatever number is there and then set the values to 0 */
-    num1 = "0";
-    num2 = "0";
+    num1 = "";
+    num2 = "";
     operator = "";
-    let values = [num1, num2, operator];
-    return num1, num2, operator;
+    console.log("Clear");
 }
